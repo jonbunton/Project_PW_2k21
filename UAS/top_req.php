77fb7b57c835178360263f932b2a2bc55626796a
KaikevinS <94227769+KaikevinS@users.noreply.github.com>
2021-12-04 16:29:11 +0700

    Update top_req.php

--- UAS/top_req.php
+++ UAS/top_req.php
@@ -1,71 +1,49 @@
 <?php
     require_once("connection.php");
 
-    if(isset($_GET["keyword"])){
-        
-        $stmt = $pdo->prepare("SELECT * FROM user WHERE nama like ?");
-        $keyword = "%".$_GET["keyword"]."%";
-        $stmt->execute([$keyword]);
+
+    $stmt = $pdo->query("SELECT * FROM pending");
+    $hist = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if(isset($_GET["detail"])){
+        $Nama_org = $_GET["detail"];
+            $stmt = $pdo->prepare("DELETE FROM pending WHERE id_pending = :Nama_org");
+
+            $result = $stmt->execute([
+            "Nama_org"=>$Nama_org
+            ]);
+
+            header("Location: top_req.php");
+    }
+
+    if(isset($_GET["detail2"])){
+        $Nama_org = $_GET["detail2"];
+
+        $stmt = $pdo->prepare("SELECT * FROM pending WHERE id_pending = :Nama_org");
+        $result = $stmt->execute(["Nama_org"=>$Nama_org]);
         $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if(isset($user)){
-        $stmt = $pdo->prepare("SELECT * FROM user WHERE email like ?");
-    $keyword = "%".$_GET["keyword"]."%";
-    $stmt->execute([$keyword]);
-    $user = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-     }
-     else{
-       $stmt = $pdo->query("SELECT * FROM history");
-       $hist = $stmt->fetchAll(PDO::FETCH_ASSOC);
-     }
-
-     if(isset($_GET["detail"])){
-
-        $cek=$_GET["detail"];
-        $stmt = $pdo->prepare("DELETE FROM namadb WHERE id = :iddb");
-
-        $result = $stmt->execute([
-        "iddb"=>$cek
-        ]);
-
-        if($result){
-        $_SESSION["message"] = "Berhasil denied ";
-        }else{
-        $_SESSION["message"] = "Gagal denied !";
+        foreach ($user as $key => $value) {
+            $email=$value['email'];
+            $waktu=$value['waktu'];
+            $tanggal=$value['tanggal'];
+            $saldo=$value['jumlah'];
         }
+
+        $stmt = $pdo->prepare("INSERT INTO history(waktu,tanggal,saldo,email) VALUES(?,?,?,?)");
+        $result = $stmt->execute([$waktu, $tanggal, $saldo, $email]);
         
+
+        $stmt = $pdo->prepare("DELETE FROM pending WHERE id_pending = :Nama_org");
+        $result = $stmt->execute(["Nama_org"=>$Nama_org]);
+
         header("Location: top_req.php");
-     }
 
-     if(isset($_GET["detail2"])){
-        $cek=$_GET["detail2"];
-        $cek=$_GET["kurang"];
-
-        $id = $_GET["edit"];
-        $stmt = $pdo->query("SELECT * FROM namadb WHERE id='$cek'");
-        $edi = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        $wkt=$edi['waktu'];
-        $tgl=$edi['tanggal'];
-        $sld=$edi['saldo'];
-        $eml=$edi['email'];
-
-        $stmt = $pdo->prepare("INSERT INTO history(waktu,tanggal, saldo, email) VALUES(?,?,?,?)");
-        $result = $stmt->execute([$wkt, $tgl, $sld, $eml]);
-        
-        
-        if($result){
-            $_SESSION["message"] = "Berhasil add nih";
-        }else{
-        $_SESSION["message"] = "Gagal add nih!";
-        }}
-     
-
-     if(isset($_SESSION["message"])){
-        echo "<script>alert('$_SESSION[message]')</script>";
-        unset($_SESSION["message"]);
     }
+    
+    if(isset($_SESSION["message"])){
+        echo "<script>alert('$_SESSION[message]')</script>";
+        unset($_SESSION["message"]);}
 ?>
 
 <!DOCTYPE html>
@@ -112,10 +90,10 @@
 								<tr class="row100 head">
 									<th class="cell100 column1">No</th>
 									<th class="cell100 column2">Email</th>
-									<th class="cell100 column3">Waktu</th>
-                                    <th class="cell100 column4">tanggal</th>
-									<th class="cell100 column5">Saldo</th>
-                                    <th class="cell100 column6">Status</th>
+									<th class="cell100 column7">Waktu</th>
+                                    <th class="cell100 column8">tanggal</th>
+									<th class="cell100 column8">Saldo</th>
+                                    <th class="cell100 column8">Status</th>
 								</tr>
 							</thead>
 						</table>
@@ -130,20 +108,19 @@
                                         foreach ($hist as $key => $value) {
                                     ?>
                                         <tr class="row100 body">
-                                            <td class="cell100 column1"><?= $value['id_history']?></td>
+                                            <td class="cell100 column1"><?= $value['id_pending']?></td>
                                             <td class="cell100 column2"><?= $value['email']?></td>
-                                            <td class="cell100 column3"><?= $value['waktu']?></td>
-                                            <td class="cell100 column4"><?= $value['tanggal']?></td>
-                                            <td class="cell100 column5"><?= $value['saldo']?></td>
-                                            <td class="cell100 column6 detailbtn">
+                                            <td class="cell100 column7"><?= $value['waktu']?></td>
+                                            <td class="cell100 column8"><?= $value['tanggal']?></td>
+                                            <td class="cell100 column8">Rp. <?= $value['jumlah']?></td>
+                                            <td class="cell100 column8 detailbtn">
                                                 <form action="#" method="get">
-                                                    <input type="hidden" name="detail" value="<?= $value['id_history']?>">
-                                                    <button>Denied</button>
+                                                    <input type="hidden" name="detail" value="<?= $value['id_pending']?>">
+                                                    <button onclick="return confirm('Denied top up?')">Denied</button>
                                                 </form>
                                                 <form action="#" method="get">
-                                                    <input type="hidden" name="detail2" value="<?= $value['id_history']?>">
-                                                    <input type="hidden" name="kurang" value="<?= $value['saldo']?>">
-                                                    <button>Accept</button>
+                                                    <input type="hidden" name="detail2" value="<?= $value['id_pending']?>">
+                                                    <button onclick="return confirm('Accept top up?')">Accept</button>
                                                 </form>
                                             </td>
                                             
@@ -164,6 +141,20 @@
                 <p class="copy">Amazake social media</p>
             </div>
         </div>
+
+        <script>
+
+            function myFunction() {
+                window.confirm("Accept the top up?");
+                location.reload();
+                
+            }
+            function myFunction2() {
+                window.confirm("Denied the top up?")
+                location.reload();
+            }
+            
+        </script>
         
 </body>
 </html>
